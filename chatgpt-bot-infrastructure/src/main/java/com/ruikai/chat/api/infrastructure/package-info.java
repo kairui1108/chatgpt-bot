1c69fa3789f65b2c36c1614f2071f4b12e397023
ruikai <ruikai@ruikai.ltd>
2023-03-31 20:35:01 +0800

    初始化DDD项目结构

---
+++ chatgpt-bot-infrastructure/src/main/java/com/ruikai/chat/api/infrastructure/package-info.java
@@ -0,0 +1,1 @@
+package com.ruikai.chat.api.infrastructure;
---
+++ chatgpt-bot-interfaces/src/main/java/com/ruikai/chat/api/Application.java
@@ -0,0 +1,18 @@
+package com.ruikai.chat.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ruikai
+ * @version 1.0
+ * @date 2023/3/31 20:20
+ */
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
