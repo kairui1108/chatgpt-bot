03f66790d7fee7db14528758b967306b104d2864
ruikai <ruikai@ruikai.ltd>
2023-04-01 21:07:27 +0800

    add task

---
+++ chatgpt-bot-application/src/main/java/com/ruikai/chat/api/application/ext/TaskRegistrarAutoConfig.java
@@ -0,0 +1,73 @@
+package com.ruikai.chat.api.application.ext;
+import com.ruikai.chat.api.application.job.ChatbotTask;
+import com.ruikai.chat.api.common.PropertyUtil;
+import com.ruikai.chat.api.domain.openai.IOpenAI;
+import com.ruikai.chat.api.domain.zsxq.IZsxqApi;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import javax.annotation.Resource;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * @author ruikai
+ * @version 1.0
+ * @date 2023/4/1 19:42
+ */
+@Configuration
+@EnableScheduling
+@Slf4j
+public class TaskRegistrarAutoConfig implements EnvironmentAware, SchedulingConfigurer {
+
+    /**
+     * 任务配置组
+     */
+    private Map<String, Map<String, Object>> taskGroupMap = new HashMap<>();
+
+    @Resource
+    private IZsxqApi zsxqApi;
+    @Resource
+    private IOpenAI openAI;
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        String prefix = "chatbot-api.";
+        String launchListStr = environment.getProperty(prefix + "launchList");
+        if (StringUtils.isEmpty(launchListStr)) {
+            return;
+        }
+        for (String groupKey : launchListStr.split(",")) {
+            Map<String, Object> taskGroupProps = PropertyUtil.handle(environment, prefix + groupKey, Map.class);
+            taskGroupMap.put(groupKey, taskGroupProps);
+        }
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        Set<String> taskGroups = taskGroupMap.keySet();
+        for (String groupKey : taskGroups) {
+            Map<String, Object> taskGroup = taskGroupMap.get(groupKey);
+            String groupName = taskGroup.get("groupName").toString();
+            String groupId = taskGroup.get("groupId").toString();
+            String cookie = taskGroup.get("cookie").toString();
+            String openAiKey = taskGroup.get("openAiKey").toString();
+            String cronExpressionBase64 = taskGroup.get("cronExpression").toString();
+            String cronExpression = new String(Base64.getDecoder().decode(cronExpressionBase64), StandardCharsets.UTF_8);
+            boolean silenced = Boolean.parseBoolean(taskGroup.get("silenced").toString());
+            log.info("创建任务 groupName：{} groupId：{} cronExpression：{}", groupName, groupId, cronExpression);
+            // 添加任务
+            taskRegistrar.addCronTask(new ChatbotTask(groupName, groupId, cookie, openAiKey, zsxqApi, openAI, silenced), cronExpression);
+        }
+    }
+
+}
---
+++ chatgpt-bot-application/src/main/java/com/ruikai/chat/api/application/job/ChatBotSchedule.java
@@ -0,0 +1,17 @@
+package com.ruikai.chat.api.application.job;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * @author ruikai
+ * @version 1.0
+ * @date 2023/4/1 20:50
+ */
+@Configuration
+@EnableScheduling
+public class ChatBotSchedule {
+
+
+
+}
